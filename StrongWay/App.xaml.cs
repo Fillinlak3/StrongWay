d4b6cab9d942b23a0a8eaaa7dcab03a9efff2999
Bucuriee - Laptop <mihai.bucur37@gmail.com>
2025-04-03 12:38:52 +0300

    Add project files.

---
+++ StrongWay/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StrongWay
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ StrongWay/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StrongWay
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
