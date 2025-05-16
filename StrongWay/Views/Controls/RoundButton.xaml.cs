8fca0429ec73b7c83629e86a1eb47aff1075bc08
Bucuriee - Laptop <mihai.bucur37@gmail.com>
2025-05-16 16:19:15 +0300

    Modifications:  - RoundButton:    - BorderBackground, StrokeColor, FontSize, TextColor, FontAttributes
    Added basic elements to WorkoutPanel

--- StrongWay/Views/Controls/RoundButton.xaml.cs
+++ StrongWay/Views/Controls/RoundButton.xaml.cs
@@ -29,7 +29,7 @@ public partial class RoundButton : ContentView
         set => SetValue(BorderBackgroundProperty, value);
     }
 
-    // 2. StrokeColor
+    // 3. StrokeColor
     public static readonly BindableProperty StrokeColorProperty =
         BindableProperty.Create(nameof(StrokeColor), typeof(Color), typeof(Border), Colors.White);
 
@@ -37,5 +37,35 @@ public partial class RoundButton : ContentView
     {
         get => (Color)GetValue(StrokeColorProperty);
         set => SetValue(StrokeColorProperty, value);
+    }
+
+    // 4. FontSize
+    public static readonly BindableProperty TextFontSizeProperty =
+        BindableProperty.Create(nameof(TextFontSize), typeof(double), typeof(RoundButton), 14.0);
+
+    public double TextFontSize
+    {
+        get => (double)GetValue(TextFontSizeProperty);
+        set => SetValue(TextFontSizeProperty, value);
+    }
+
+    // 5. FontWeight (FontAttributes)
+    public static readonly BindableProperty TextFontAttributesProperty =
+        BindableProperty.Create(nameof(TextFontAttributes), typeof(FontAttributes), typeof(RoundButton), FontAttributes.None);
+
+    public FontAttributes TextFontAttributes
+    {
+        get => (FontAttributes)GetValue(TextFontAttributesProperty);
+        set => SetValue(TextFontAttributesProperty, value);
+    }
+
+    // 2. TextColor
+    public static readonly BindableProperty TextForegroundColorProperty =
+        BindableProperty.Create(nameof(TextForegroundColor), typeof(Color), typeof(RoundButton), Color.FromArgb("#FFFFFF"));
+
+    public Color TextForegroundColor
+    {
+        get => (Color)GetValue(TextForegroundColorProperty);
+        set => SetValue(TextForegroundColorProperty, value);
     }
 }
